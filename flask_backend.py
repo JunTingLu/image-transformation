0299fa380de05601197afdf983ded367ce0298ce
JunTingLu <juntinglu84@gmail.com>
2023-08-11 00:55:21 +0800

    Adjust code

--- flask_backend.py
+++ flask_backend.py
@@ -1,10 +1,8 @@
-import os
 from flask import Flask
-from flask import request,jsonify,send_file
+from flask import request,send_file
 # from flask_uploads import UploadSet, IMAGES
 import base64 
 import numpy as np
-import cv2
 from torchvision import transforms
 from PIL import Image
 import io
@@ -46,23 +44,12 @@ def preprocess_img(data):
     return img2arr
 
 
-""" 將轉換照片回傳前端"""
-def show_img(input_img):
-    # 將CUDA Tensor複製到主機內存
-    input_bytes=torch.tensor(input_img).cpu().numpy()
-    print(64,input_bytes)
-    # 暫存圖片的二進位資料
-    buffer=io.BytesIO()
-    buffer.write(input_bytes)
-    # 重新將資料讀寫指針移動到初始位置
-    buffer.seek(0)
-    # 將圖轉為base64編碼
-    img_b64=base64.b64encode(buffer.getvalue()).decode()
-    return  img_b64
+# Judge the channel RGB
+def check_RGB(input):
+    pass
 
 
-""" Model evaluation"""
-# model train
+# Model train
 def model_generate(origin_img,style_img):
     save_dir="/logs_result/nst_cnn_model.pth"
     # Transform style_img to array
@@ -78,8 +65,9 @@ def model_generate(origin_img,style_img):
         total_loss.backward()
         optimizer.step() # update gen_img parameters
         if e==epoch-1:
-            save_image(gen_img,"nst_{}.png".format(e))
-            return  gen_img
+            save_path="nst_{}.png".format(e)
+            save_image(gen_img,save_path)
+            return  save_path
     with open(save_dir) as f:
         torch.load(model.state_dict(), f)
 
@@ -95,22 +83,18 @@ def image_loader(input_img):
 
 @app.route('/img_backend', methods=['GET', 'POST'])
 def upload_data():
-    path_list=[]
-    # 初始化img_b64為空值
-    img_b64=""
-    img_process=""
     if request.method=='POST': 
         file=request.form['image']
         style=request.form['style']
-        # 圖像處理
+        # Preprocessing the image before training
         resized_img=preprocess_img(file)
         resized_img=image_loader(resized_img)
         # Decided the style image as input
         style_img=Image.open("output/style/{}.png".format(style))
         style_img=image_loader(style_img)
         # Generate the output image
-        path=model_generate(resized_img,style_img)
-        return send_file(path, mimetype='image/png')
+        save_path=model_generate(resized_img,style_img)
+        return send_file(save_path, mimetype='image/png')
 
 
 if __name__ == '__main__':
